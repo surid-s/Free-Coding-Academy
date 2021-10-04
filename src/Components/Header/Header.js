dce895b8b4fe58505d5305a57a6f6459a6a976f3
surid-s <maruf15-9549@diu.edu.bd>
2021-10-04 10:41:03 +0600

    Components added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Button variant="primary">Hello</Button>
+        </div>
+    );
+};
+
+export default Header;
